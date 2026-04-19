e6d313e1c96806599f42fc461ca65f4663bdfc9a
nahn945 <yukikobayashie97@gmail.com>
2026-04-19 21:06:22 +0900

    Feat: alpha03

--- GameScene/player.gd
+++ GameScene/player.gd
@@ -1,103 +1,157 @@
 extends RigidBody2D
-signal damaged(value : float, id : int)
-signal damage_end(id : int)
+signal tackled
 
-@export var id : int
-@export var power : float = 0.5
+@export var input_up : String = "ui_up"
+@export var input_left : String = "ui_left"
+@export var input_right : String = "ui_right"
 
-@export var input_up : String
-@export var input_left : String
-@export var input_right : String
+@onready var jump_timer : Timer = $JumpTimer
+@onready var effect_timer : Timer = $EffectTimer
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 
-const ROT_SPEED : float = 5.0
-const JUMP_SPEED : float = 800.0
-const JUMP_CHARGE : float = 0.01
+const BOUNCE_COEFFICIENT : float = 0.8
+const DAMAGED_COLOR : Color = Color.RED
+const NORMAL_COLOR : Color = Color.WHITE
 
-const X_MAX : float = 1920
-const X_MIN : float = 0.0
-const Y_MAX : float = 1080
-const Y_MIN : float = 0.0
-const CENTER_X : float = 960
-const CENTER_Y : float = 540
-
-@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
-@onready var particle : CPUParticles2D = $CPUParticles2D
-
-var can_operate : bool = true
+var is_active : bool = true
+var is_jumping : bool = false
+var is_bounding : bool = false
+var is_attacked_front : bool = false
+var is_attacked_rear : bool = false
 
 var jump_parameter : float = 0.0
-var can_jump : bool = true
+var rot_parameter : float = 1.0
 
-var damage_rate : float = 0.0
+var rear_damage : float = 0.0
 
-var is_attackikng : bool = false
+var minimum_tackle_speed : float = 0.0
 
-func _ready() -> void:
-	anim.animation = "default"
+# status
+var hp : float = 100.0
+var attack_front : float = 2.0
+var attack_rear : float = 5.0
+
+var max_rot_speed : float = 10.0
+var rot_acceleration : float = 5.0
+
+var max_jump_speed : float = 500.0
+var jump_acceleration : float = 10.0
+
+func _init() -> void:
+	minimum_tackle_speed = max_jump_speed / 2.0
+
+func get_attack_front() -> float:
+	return attack_front
+
+func get_attack_rear() -> float:
+	return attack_rear
+
+func change_activation(flag : bool):
+	is_active = flag
+
+func apply_damage(value : float):
+	modulate = DAMAGED_COLOR
+	hp -= value
+
+func wait_jump_end():
+	jump_timer.start()
+	await jump_timer.timeout
+	jump_timer.stop()
 
 func _physics_process(delta: float) -> void:
-	if sleeping:
+	if !is_active:
+		set_deferred("freeze", true)
 		return
 	
-	if Input.is_action_pressed(input_up) and can_jump:
-		anim.animation = "charge"
-		jump_parameter = min(1.0, jump_parameter + JUMP_CHARGE)
-		
-	if Input.is_action_just_released(input_up) and can_jump:
-		anim.animation = "boost"
-		can_jump = false
-		var jump_force := Vector2.UP.rotated(transform.get_rotation()) * JUMP_SPEED * jump_parameter 
-		apply_impulse(jump_force)
-		await get_tree().create_timer(max(0.5, jump_parameter * 1.5)).timeout
-		jump_parameter = 0.0
-		can_jump = true
-		anim.animation = "default"
-		
-	check_is_outside()
+	set_deferred("freeze", false)
 	
-	var direction : float = Input.get_axis(input_left, input_right)
-	if direction:
-		angular_velocity = direction * ROT_SPEED
-
-func check_is_outside():
-	if global_position.x < X_MIN or global_position.x > X_MAX:
-		global_position = Vector2(CENTER_X, global_position.y)
-		damaged.emit(1, id)
-		await get_tree().create_timer(0.1).timeout
-		damage_end.emit(id)
-	if global_position.y < Y_MIN or global_position.y > Y_MAX:
-		global_position = Vector2(global_position.x, CENTER_Y)
-		damaged.emit(1, id)
-		await get_tree().create_timer(0.1).timeout
-		damage_end.emit(id)
+	if !is_jumping:
+		if Input.is_action_pressed(input_up):
+			animation.animation = "charge"
+			jump_parameter += jump_acceleration
+			if jump_parameter > max_jump_speed:
+				jump_parameter = max_jump_speed
+		
+		if Input.is_action_just_released(input_up):
+			animation.animation = "boost"
+			is_jumping = true
+			apply_impulse(jump_parameter * Vector2.UP.rotated(rotation))
+			await wait_jump_end()
+			animation.animation = "default"
+			jump_parameter = 0.0
+			is_jumping = false
 	
+	var rot_direction : float = Input.get_axis(input_left, input_right)
+	if rot_direction != 0.0:
+		
+		set_deferred("angular_velocity", rot_direction * rot_parameter)
+		rot_parameter = min(rot_parameter + rot_acceleration * delta, max_rot_speed)
+	else:
+		rot_parameter = 1.0
 
 func _process(delta: float) -> void:
-	if is_attackikng and id:
-		damaged.emit(damage_rate, id)
+	if is_attacked_rear:
+		apply_damage(rear_damage)
 
-func _on_area_entered(area: Area2D) -> void:
-	is_attackikng = true
-	damage_rate = power
+func tackle_impulse(velocity : Vector2, opponent : RigidBody2D):
+	set_deferred("lock_rotation", true)
+	set_deferred("freeze", true)
+	opponent.set_deferred("freeze", true)
+	set_deferred("linear_velocity", Vector2.ZERO)
+	
+	effect_timer.start()
+	await effect_timer.timeout
+	effect_timer.stop()
+	
+	set_deferred("freeze", false)
+	set_deferred("lock_rotation", false)
+	
+	apply_impulse(velocity)
+	
+	opponent.set_deferred("freeze", false)
+	
 
-func _on_area_exited(area: Area2D) -> void:
-	is_attackikng = false
-	damage_end.emit(id)
+# attack
+func _on_damage_area_area_entered(area: Area2D) -> void:
+	var opponent : RigidBody2D = area.get_parent()
+	if area.is_in_group("attack_front")\
+	and opponent.linear_velocity.length() >= minimum_tackle_speed:
+		is_attacked_front = true
+		apply_damage(opponent.get_attack_rear())
+		var tackle_velocity : Vector2 = opponent.linear_velocity
+		await tackle_impulse(tackle_velocity, opponent)
+		
+		is_attacked_front = false
+		
+	elif area.is_in_group("attack_rear"):
+		rear_damage = opponent.get_attack_rear()
+		is_attacked_rear = true
 
-func stop_operating():
-	can_operate = false
-	linear_velocity = Vector2.ZERO
-	angular_velocity = 0.0
-	gravity_scale = 0.0
-	self.sleeping = true
+func _on_damage_area_area_exited(area : Area2D) -> void:
+	var opponent : RigidBody2D = area.get_parent()
+	if area.is_in_group("attack_rear"):
+		modulate = NORMAL_COLOR
+		is_attacked_rear = false
 
-func activate_operating():
-	can_operate = true
-	self.sleeping = false
-	gravity_scale = 0.05
-	apply_impulse(Vector2.DOWN)
+# bounce
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	var shape_owner : int = shape_find_owner(local_shape_index)
+	var shape : Object = shape_owner_get_owner(shape_owner)
+	var opponent : RigidBody2D = shape.get_parent()
+	if shape is CollisionPolygon2D\
+	and !is_bounding\
+	and !is_attacked_front\
+	and linear_velocity.length() >= opponent.linear_velocity.length():
+		is_bounding = true
+		set_deferred("linear_velocity", linear_velocity * 1 * BOUNCE_COEFFICIENT)
+		opponent.set_deferred("linear_velocity", linear_velocity * BOUNCE_COEFFICIENT * -1)
 
-func lose():
-	particle.emitting = true
-	await get_tree().create_timer(1.0).timeout
-	hide()
+
+
+func _on_body_shape_exited(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	var shape_owner : int = shape_find_owner(local_shape_index)
+	var shape : Object = shape_owner_get_owner(shape_owner)
+	
+	if shape is CollisionPolygon2D\
+	and is_bounding:
+		is_bounding = false
